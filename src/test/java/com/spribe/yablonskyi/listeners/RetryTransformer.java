9d0ff48591bec5106fc1dae78bba0c8b9fc8d003
volodymyry <vladimir.yablonsky@gmail.com>
2025-08-07 22:32:55 +0300

    refactoring

--- src/test/java/com/spribe/yablonskyi/listeners/RetryTransformer.java
+++ src/test/java/com/spribe/yablonskyi/listeners/RetryTransformer.java
@@ -8,10 +8,7 @@ import java.lang.reflect.Method;
 
 public class RetryTransformer implements IAnnotationTransformer {
     @Override
-    public void transform(ITestAnnotation annotation,
-                          Class testClass,
-                          Constructor testConstructor,
-                          Method testMethod) {
+    public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
         if (annotation.getRetryAnalyzerClass() == null) {
             annotation.setRetryAnalyzer(RetryAnalyzer.class);
         }
